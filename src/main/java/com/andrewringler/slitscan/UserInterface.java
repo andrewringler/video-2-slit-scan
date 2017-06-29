ba9a495af3c648d86488c65263f7d28b9e275d17
Andrew Ringler <andrew@andrewringler.com>
2017-06-29 19:32:43 -0400

    starting frame and width and UI

--- src/main/java/com/andrewringler/slitscan/UserInterface.java
+++ src/main/java/com/andrewringler/slitscan/UserInterface.java
@@ -1,5 +1,7 @@
 package com.andrewringler.slitscan;
 
+import java.io.File;
+
 import controlP5.CallbackEvent;
 import controlP5.CallbackListener;
 import controlP5.ControlFont;
@@ -20,6 +22,9 @@ public class UserInterface {
 	private final Slider generationProgressSlider;
 	private final RadioButton playSpeed;
 	private final Textfield outputFileLabel;
+	private final Textfield startPixelField;
+	private final Textfield imageWidthField;
+	private final Textfield imageHeightField;
 	
 	public boolean draggingSlit = false;
 	public float SLIT_LOCATION = 0.5f; // [0-1]
@@ -28,12 +33,12 @@ public class UserInterface {
 		this.p = p;
 		cp5 = new ControlP5(p);
 		
-		PFont pfont = p.createFont("RobotoMono-Medium", 11, true); // use true/false for smooth/no-smooth
-		ControlFont font = new ControlFont(pfont, 11);
+		PFont pfont = p.createFont("RobotoMono-Medium", 10, true); // use true/false for smooth/no-smooth
+		ControlFont font = new ControlFont(pfont, 10);
 		cp5.setFont(font);
 		
 		// Video load/setup
-		Group videoSettingsUI = cp5.addGroup("Video").setPosition(10, 30).setBackgroundHeight(165).setWidth(400).setBackgroundColor(p.color(30, 30, 30, 240)).setBarHeight(20);
+		Group videoSettingsUI = cp5.addGroup("Video").setMoveable(true).setPosition(10, 30).setBackgroundHeight(165).setWidth(400).setBackgroundColor(p.color(30, 30, 30, 240)).setBarHeight(20);
 		cp5.addButton("selectVideoFile").setLabel("Open video file").setPosition(10, 10).setSize(120, 20).setGroup(videoSettingsUI).onClick(new CallbackListener() {
 			@Override
 			public void controlEvent(CallbackEvent arg0) {
@@ -41,8 +46,8 @@ public class UserInterface {
 			}
 		});
 		videoFileLabel = cp5.addTextfield("videoFileTextField").setLabel("Video File Path").setPosition(10, 40).setSize(350, 20).setGroup(videoSettingsUI);
-		videoFileDuration = cp5.addTextfield("videoFileDuration").setLabel("Duration (s)").setValue("0").setInputFilter(ControlP5.FLOAT).setPosition(10, 95).setSize(60, 20).setUserInteraction(false).setGroup(videoSettingsUI);
-		videoFrameCountField = cp5.addTextfield("videoFileFrameCount").setLabel("Total Frames").setValue("0").setInputFilter(ControlP5.INTEGER).setPosition(160, 95).setSize(60, 20).setUserInteraction(false).setGroup(videoSettingsUI);
+		videoFileDuration = cp5.addTextfield("videoFileDuration").setLabel("Duration (s)").setValue("0").setInputFilter(ControlP5.FLOAT).setPosition(10, 95).setSize(60, 20).setUserInteraction(false).setAutoClear(false).setGroup(videoSettingsUI);
+		videoFrameCountField = cp5.addTextfield("videoFileFrameCount").setLabel("Total Frames").setValue("0").setInputFilter(ControlP5.INTEGER).setAutoClear(false).setPosition(160, 95).setSize(60, 20).setGroup(videoSettingsUI);
 		videoFPSField = cp5.addTextfield("videoFileFPS").setLabel("FPS").setInputFilter(ControlP5.FLOAT).setValue("30").setPosition(100, 95).setSize(30, 20).setAutoClear(false).setGroup(videoSettingsUI);
 		videoFPSField.onChange(new CallbackListener() {
 			@Override
@@ -50,11 +55,12 @@ public class UserInterface {
 				float fps = Float.valueOf(videoFPSField.getText());
 				float duration = Float.valueOf(videoFileDuration.getText());
 				videoFrameCountField.setValue(String.valueOf((int) (fps * duration)));
+				videoFrameCountUpdated();
 			}
 		});
 		
 		// Slit scan setup/run
-		Group slitGenerationUI = cp5.addGroup("Slit-Scan").setPosition(10, 230).setBackgroundHeight(200).setWidth(400).setBackgroundColor(p.color(30, 30, 30, 240)).setBarHeight(20);
+		Group slitGenerationUI = cp5.addGroup("Slit-Scan").setMoveable(true).setPosition(10, 230).setBackgroundHeight(250).setWidth(400).setBackgroundColor(p.color(30, 30, 30, 240)).setBarHeight(20);
 		cp5.addTextlabel("playSpeedLabel").setText("Play Speed").setPosition(6, 10).align(ControlP5.LEFT_OUTSIDE, ControlP5.BASELINE, ControlP5.LEFT_OUTSIDE, ControlP5.BASELINE).setSize(30, 20).setGroup(slitGenerationUI);
 		playSpeed = cp5.addRadioButton("chooseRenderSpeed").setPosition(10, 25).setNoneSelectedAllowed(true).setItemsPerRow(4).setSpacingColumn(20).addItem("1x", 1).addItem("2x", 2).addItem("4x", 4).addItem("8x", 8).setGroup(slitGenerationUI);
 		playSpeed.activate(0);
@@ -65,15 +71,39 @@ public class UserInterface {
 				p.outputFileSelector();
 			}
 		});
-		outputFileLabel = cp5.addTextfield("outputFileLabel").setLabel("Output File Path").setPosition(10, 70).setSize(350, 20).setGroup(slitGenerationUI);
+		outputFileLabel = cp5.addTextfield("outputFileLabel").setLabel("Output File Path").setPosition(10, 70).setSize(350, 20).setAutoClear(false).setGroup(slitGenerationUI);
+		outputFileLabel.onChange(new CallbackListener() {
+			@Override
+			public void controlEvent(CallbackEvent theEvent) {
+				// verify
+				p.outputFileSelected(new File(outputFileLabel.getText()));
+			}
+		});
 		
-		cp5.addButton("Generate slit-scan image").setPosition(10, 150).setSize(200, 20).setGroup(slitGenerationUI).onClick(new CallbackListener() {
+		startPixelField = cp5.addTextfield("startFrameField").setLabel("Start Pixel").setText("0").setInputFilter(ControlP5.INTEGER).setAutoClear(false).setPosition(10, 120).setSize(60, 20).setGroup(slitGenerationUI);
+		startPixelField.onChange(new CallbackListener() {
+			@Override
+			public void controlEvent(CallbackEvent theEvent) {
+				Integer startingPixel = Integer.valueOf(startPixelField.getText());
+				Integer imageWidth = Integer.valueOf(imageWidthField.getText());
+				if (startingPixel + p.SLIT_WIDTH >= imageWidth) {
+					startPixelField.setText(String.valueOf(imageWidth - p.SLIT_WIDTH));
+				}
+				if (startingPixel < 0) {
+					startPixelField.setText("0");
+				}
+			}
+		});
+		imageWidthField = cp5.addTextfield("imageWidthField").setLabel("Width").setText("0").setInputFilter(ControlP5.INTEGER).setAutoClear(false).setUserInteraction(true).setPosition(100, 120).setSize(60, 20).setGroup(slitGenerationUI);
+		imageHeightField = cp5.addTextfield("imageHeightField").setLabel("Height").setText("0").setInputFilter(ControlP5.INTEGER).setAutoClear(false).setUserInteraction(false).setPosition(170, 120).setSize(60, 20).setGroup(slitGenerationUI);
+		
+		cp5.addButton("Generate slit-scan image").setPosition(10, 200).setSize(200, 20).setGroup(slitGenerationUI).onClick(new CallbackListener() {
 			@Override
 			public void controlEvent(CallbackEvent arg0) {
 				p.generateSlitScan();
 			}
 		});
-		generationProgressSlider = cp5.addSlider("progress").setLabel("Progress").setPosition(10, 180).setSize(300, 15).setRange(0, 100).setUserInteraction(false).setGroup(slitGenerationUI);
+		generationProgressSlider = cp5.addSlider("progress").setLabel("Progress").setPosition(10, 230).setSize(300, 15).setRange(0, 100).setUserInteraction(false).setGroup(slitGenerationUI);
 	}
 	
 	public float getPlaySpeed() {
@@ -109,7 +139,8 @@ public class UserInterface {
 		videoFileDuration.setValue(String.valueOf(videoDuration));
 		
 		float fps = Float.valueOf(videoFPSField.getText());
-		videoFrameCountField.setText(String.valueOf((int) (fps * videoDuration)));
+		videoFrameCountField.setValue(String.valueOf((int) (fps * videoDuration)));
+		videoFrameCountUpdated();
 	}
 	
 	public Integer getTotalVideoFrames() {
@@ -122,5 +153,23 @@ public class UserInterface {
 	
 	public void outputFileSelected(String outputFile) {
 		outputFileLabel.setValue(outputFile);
+	}
+	
+	private void videoFrameCountUpdated() {
+		Integer frameCount = Integer.valueOf(videoFrameCountField.getText());
+		Integer startingPixel = Integer.valueOf(startPixelField.getText());
+		if (startingPixel + p.SLIT_WIDTH >= frameCount) {
+			startPixelField.setText(String.valueOf(frameCount - p.SLIT_WIDTH));
+		}
+		imageWidthField.setText(String.valueOf(frameCount * p.SLIT_WIDTH));
+		imageHeightField.setText(String.valueOf(p.video.height));
+	}
+	
+	public Integer getStartingPixel() {
+		return Integer.valueOf(startPixelField.getText());
+	}
+	
+	public Integer getImageWidth() {
+		return Integer.valueOf(imageWidthField.getText());
 	}
 }
